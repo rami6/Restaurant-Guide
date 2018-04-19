167ae04dfaf179463dd4abb6dfeae44fcf6c2d5e
rami6 <30137645+rami6@users.noreply.github.com>
2018-04-19 00:44:35 -0700

    Add context variable

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -3,20 +3,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_old(request):
-	html_var = 'strings'
-	html_ = f"""<!DOCTYPE html>
-	<html lang=en>
-	<head>
-	</head>
-	<body>
-	<h1>Hello World</h1>
-	<p>This is {html_var} html</p>
-	</body>
-	</html>
-	"""
-	return HttpResponse(html_)
-
 def home(request):
-	num = random.randint(0, 100000000)
-	return render(request, "base.html", {"html_var": "context variable", "num": num})
+	num = None
+	some_list = [random.randint(0, 100000000), random.randint(0, 100000000), random.randint(0, 100000000)]
+	condition_bool_item = True
+	if condition_bool_item:
+		num = random.randint(0, 100000000);
+	context = {
+		"num": num, 
+		"some_list": some_list
+	}
+	return render(request, "base.html", context) 
