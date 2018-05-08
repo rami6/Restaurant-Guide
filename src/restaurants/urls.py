508763931e079da54823d626f265c8997558a5ff
rami6 <30137645+rami6@users.noreply.github.com>
2018-05-07 23:02:16 -0700

    Change urls to use name

---
+++ src/restaurants/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+
+
+from restaurants.views import (
+    # restaurant_listview,
+    # restaurant_createview
+    RestaurantListView,
+    RestaurantDetailView,
+    RestaurantCreateView,
+)
+
+urlpatterns = [
+    url(r'^$', RestaurantListView.as_view(), name='list'),
+    url(r'^create/$', RestaurantCreateView.as_view(), name='create'),
+    url(r'^(?P<slug>[\w-]+)/$', RestaurantDetailView.as_view(), name='detail'),
+]
